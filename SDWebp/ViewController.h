672d8852a1708e86372906b95e56d860317274b0
suzhiqiu <suzhiqiu@linggan.com>
2017-07-31 16:39:53 +0800

    Initial Commit

---
+++ SDWebp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SDWebp
+//
+//  Created by suzq on 2017/7/31.
+//  Copyright © 2017年 suzq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SDWebp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SDWebp
+//
+//  Created by suzq on 2017/7/31.
+//  Copyright © 2017年 suzq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
